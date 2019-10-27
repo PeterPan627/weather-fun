0bd3888c7d0672f9aa5c470ae953814b0ce846da
LeiMing627 <jackperry933jp@gmail.com>
2019-10-27 22:48:57 +0000

    read dataset into pandas dataframe

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import requests
+import pandas as pd 
 
-url = "https://www.cl.cam.ac.uk/research/dtg/weather/weather-raw.csv"
-response = requests.get(url)
-print(response.text)
+df = pd.read_csv("weather-raw.csv", header=None)
