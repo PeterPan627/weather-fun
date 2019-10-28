bdeae99b85ea286f02974e4ca6425243c1b5f32b
LeiMing627 <jackperry933jp@gmail.com>
2019-10-28 14:16:16 +0000

    Read in to Pandas

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
 import csv
+import pandas
+
 from helper_functions import save_csv_from_url
 
 url = "https://www.cl.cam.ac.uk/research/dtg/weather/weather-raw.csv"
 data_file = "data.csv"
 
 save_csv_from_url(url, data_file)
+
+headers = ["Timestamp","Temperature","Humidity","DewPoint","Pressure",
+           "MeanWindSpeed","WindBearing","Sunshine","Rainfall","MaxWindSpeed"]
+
+df = pandas.read_csv(data_file, names=headers)
+
+print(df)
