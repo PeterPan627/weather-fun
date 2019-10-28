3e532edaa7b76105894c8105e742b6f11c646526
LeiMing627 <jackperry933jp@gmail.com>
2019-10-28 14:25:25 +0000

    Only save it file does not exist

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import requests
 import csv
 import pandas
+import os
 
 from helper_functions import save_csv_from_url
 
 url = "https://www.cl.cam.ac.uk/research/dtg/weather/weather-raw.csv"
 data_file = "data.csv"
 
-save_csv_from_url(url, data_file)
+if not os.path.exists(data_file):
+	save_csv_from_url(url, data_file)
 
 headers = ["Timestamp","Temperature","Humidity","DewPoint","Pressure",
-           "MeanWindSpeed","WindBearing","Sunshine","Rainfall","MaxWindSpeed"]
+		   "MeanWindSpeed","WindBearing","Sunshine","Rainfall","MaxWindSpeed"]
 
 df = pandas.read_csv(data_file, names=headers)
 
