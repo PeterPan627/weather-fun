b61a4cd9da192ae54283045a9b9103516cd1c5e5
LeiMing627 <jackperry933jp@gmail.com>
2019-11-04 13:23:37 +0000

    corrected wind function

--- normalise_functions.py
+++ normalise_functions.py
@@ -1,4 +1,4 @@
-#functions to normalise sun, rain and wind
+#functions to normalize sun, rain and wind
 
 def normalize_sun(df):
     result = df.copy()
@@ -11,7 +11,7 @@ def normalize_wind(df):
     result = df.copy()
     max_value = df["MeanWindSpeed"].max()
     min_value = df["MeanWindSpeed"].min()
-    result["MeanWindSpeed"] = (df["Sunshine"] - min_value) / (max_value - min_value)
+    result["MeanWindSpeed"] = (df["MeanWindSpeed"] - min_value) / (max_value - min_value)
     
     max_value = df["MaxWindSpeed"].max()
     min_value = df["MaxWindSpeed"].min()
